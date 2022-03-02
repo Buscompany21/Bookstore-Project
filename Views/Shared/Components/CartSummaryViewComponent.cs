ed6f13726fe44f937ce25e9135328177b5234aa8
Buscompany21 <buscobrian1@gmail.com>
2022-03-02 16:20:44 -0700

    Added Cart Icon

---
+++ Views/Shared/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission_7_Assignment.Models;
+namespace SportsStore.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Basket basket;
+        public CartSummaryViewComponent(Basket basketService)
+        {
+            basket = basketService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(basket);
+        }
+    }
+}
