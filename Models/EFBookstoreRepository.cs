a0db6eac55077a8ffaf0b2921ba560522ad4e9da
Buscompany21 <buscobrian1@gmail.com>
2022-03-09 18:02:59 -0700

    Finished class 2

--- Models/EFBookstoreRepository.cs
+++ Models/EFBookstoreRepository.cs
@@ -13,5 +13,22 @@ namespace Mission_7_Assignment.Models
             context = temp;
         }
         public IQueryable<Book> Books => context.Books;
+
+        public void SaveBook(Book b)
+        {
+            context.SaveChanges();
+        }
+
+        public void CreateBook(Book b)
+        {
+            context.Add(b);
+            context.SaveChanges();
+        }
+
+        public void DeleteBook(Book b)
+        {
+            context.Remove(b);
+            context.SaveChanges();
+        }
     }
 }
--- Models/IBookstoreRepository.cs
+++ Models/IBookstoreRepository.cs
@@ -8,5 +8,9 @@ namespace Mission_7_Assignment.Models
     public interface IBookstoreRepository
     {
         IQueryable<Book> Books { get; }
+
+        public void SaveBook(Book b);
+        public void CreateBook(Book b);
+        public void DeleteBook(Book b);
     }
 }
